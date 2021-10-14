0e4d06b098ba37e06314574fb64892605415f7da
roblcr <lecullier.robin@gmail.com>
2021-10-14 17:22:13 +0200

    first step

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// DOM selection 
+const list = 'élément #list'
+const input = 'élément #input'
+const add = 'élément #add'
+const clear = 'élémnet #clear'
+const url = 'élement #url'
+const load = 'élément #load'
