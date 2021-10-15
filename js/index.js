f575615d26e37a57aebf6f02c6bff74a7dbbbb50
roblcr <lecullier.robin@gmail.com>
2021-10-15 17:07:32 +0200

    récupérations des différents éléments du DOM

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 // DOM selection 
-const list = 'élément #list'
-const input = 'élément #input'
-const add = 'élément #add'
-const clear = 'élémnet #clear'
-const url = 'élement #url'
-const load = 'élément #load'
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+const add = document.getElementById('add');
+const clear = document.getElementById('clear');
+const url = document.getElementById('url');
+const load = document.getElementById('load');
+console.log(list, input, add, clear, url, load)
+// on récupère le tableau des taches existantes
+const tasks = ['Faire du sport', 'faire le ménage', 'faire les courses'];
+console.log(tasks);
+
+
 
